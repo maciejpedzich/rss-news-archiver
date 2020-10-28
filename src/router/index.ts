61906c84510f63c2ba3734b438e9efaa72abdf02
Maciej Pędzich <dnalop.me@gmail.com>
2020-10-28 20:27:34 +0100

    Create about page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
+
 import Home from '../views/Home.vue';
+import About from '../views/About.vue';
 
 Vue.use(VueRouter);
 
@@ -9,6 +11,11 @@ const routes: Array<RouteConfig> = [
     path: '/',
     name: 'Home',
     component: Home,
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About,
   },
 ];
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="text-center mt-3">
+    <h1 class="text-h2 mb-4">RSS News Archiver</h1>
+    <h5 class="text-h5">Made by Maciej Pędzich, 2020</h5>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({})
+export default class About extends Vue {}
+</script>
